09fb82224f67516013a5bac7c484fe1b5dc745c5
edvaz14 <edwindvazquez@yahoo.com>
2015-02-06 17:42:20 -0500

    Created Test2.java

---
+++ Test/src/test/Test2.java
@@ -0,0 +1,24 @@
+/**
+ * Change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test;
+
+/**
+ *
+ * @author Edwin
+ */
+public class Test2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+//Comment added to file	    
+//added in Netbeans b
+}
+
